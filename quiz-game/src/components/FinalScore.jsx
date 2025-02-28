b79a1017eb302426cf96b0b4743a22e5ab0e7308
vong <vongsouvan09@gmail.com>
2025-02-28 12:45:43 +0700

    create final score component

--- quiz-game/src/components/FinalScore.jsx
+++ quiz-game/src/components/FinalScore.jsx
@@ -1,5 +1,37 @@
-export default function FinalScore(){
+export default function FinalScore({
+  quizQuestions,
+  score
+}){
   return(
-    <div></div>
+    <>
+      <div className="border border-solid border-cs-black text-center px-4 py-4 rounded-2xl shadow-lg">
+        <h1 className="text-3xl font-bold underline underline-offset-8 mb-4">Final Score</h1>
+        <div className="font-bold text-2xl text-[#e99720]">{`${score} / ${quizQuestions.length}`}</div>
+      </div>
+      {quizQuestions.map((quizQuestion, index) => (
+        <div key={`Question ${index}`} className="my-8">
+          <h2 className="text-2xl font-bold mb-4">{quizQuestion.question}</h2>
+          <ul className="w-full flex flex-col gap-4" role="list">
+            {quizQuestion.choices.map((choice, index) => (
+              <li key={`answer choice ${index + 1}: ${choice}`} role="list-item">
+                <button
+                  className={`quiz-choice ${quizQuestion.correct === choice ? "correct" : "inactive"}`}
+                  disabled={true}
+                  role="button"
+                  aria-labelledby={`choice-${index}`}
+                >
+                  <p
+                    id={`choice-${index}`}
+                    className={`choice-text`}
+                  >
+                    {choice}
+                  </p>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
   )
 }
--- quiz-game/src/components/QuizQuestion.jsx
+++ quiz-game/src/components/QuizQuestion.jsx
@@ -1,38 +1,40 @@
 export default function QuizQuestion({
-  quizData,
+  quizQuestions,
   isAnswered,
   currentQuestionIndex,
   answeredCount,
   selectedAnswer,
   remainingTime,
   setIsAnswered,
-  setIsTimerRunning,
+  setIsFinished,
+  setIsTimerActive,
   setCurrentQuestionIndex,
   setAnsweredCount,
   setSelectedAnswer,
   setScore,
-  setRemaining
+  setRemainingTime
 }){
 
-  function handleAnswerQuestion(choice){
-    const correctAnswer = quizData[currentQuestionIndex].correct;
+  function handleSelectAnswer(choice){
+    const correctAnswer = quizQuestions[currentQuestionIndex].correct;
     const correct = correctAnswer === choice;
     if(correct){
       setScore(prevCount => prevCount + 1);
     }
-    setIsTimerRunning(false);
+    setIsTimerActive(false);
     setAnsweredCount(prevCount => prevCount + 1);
     setSelectedAnswer(choice);
     setIsAnswered(true);
   }
 
   function handleNextQuestion(){
-    if(currentQuestionIndex >= quizData.length){
+    console.log(currentQuestionIndex)
+    if(currentQuestionIndex >= quizQuestions.length - 1){
       setIsFinished(true);
       return;
     }
-    setIsTimerRunning(true);
-    setRemaining(1 * 60);
+    setIsTimerActive(true);
+    setRemainingTime(1 * 60);
     setCurrentQuestionIndex(prevIndex => prevIndex + 1);
     setSelectedAnswer(null);
     setIsAnswered(false);
@@ -46,30 +48,29 @@ export default function QuizQuestion({
 
   return(
     <div className="flex flex-col justify-center items-center gap-4">
-      <h1 className="font-bold text-3xl tracking-widest">Math quiz</h1>
+      <h2 className="font-bold text-3xl tracking-widest">Math quiz</h2>
       <div className="w-full">
         <div className="w0full h-1 bg-cs-black rounded-full overflow-x-hidden">
           <div
             className="bg-cs-grey h-full transition-all duration-300"
             style={{
-              width:`${(answeredCount * 100) / quizData.length}%`
+              width:`${(answeredCount * 100) / quizQuestions.length}%`
             }}
           >
           </div>
         </div>
-        <p className="text-xs my-1">{`${answeredCount} / ${quizData.length}`}</p>
+        <p className="text-xs my-1">{`${answeredCount} / ${quizQuestions.length}`}</p>
       </div>
-      <h2 className="text-4xl tracking-wide">{quizData[currentQuestionIndex].question}</h2>
-      <ul className="w-full flex flex-col gap-4" role="list">
-        {quizData[currentQuestionIndex].choices.map((choice, index) => (
+      <h2 className="text-4xl tracking-wide text-center mb-8">{quizQuestions[currentQuestionIndex].question}</h2>
+      <ul className="max-w-96 w-full flex flex-col gap-4" role="list">
+        {quizQuestions[currentQuestionIndex].choices.map((choice, index) => (
           <li key={`answer choice ${index + 1}: ${choice}`} role="list-item">
             <button
-              className={`choice-button ${selectedAnswer ?  choice === quizData[currentQuestionIndex].correct ? "correct" : selectedAnswer === choice ? "wrong" : "unselected" : ""}`}
-              disabled={isAnswered}
-              aria-label={`Answer choice ${index + 1}: ${choice}`}
+              className={`quiz-choice ${selectedAnswer ?  choice === quizQuestions[currentQuestionIndex].correct ? "correct" : selectedAnswer === choice ? "wrong" : "inactive" : ""}`}
+              aria-disabled={isAnswered}
               role="button"
               aria-labelledby={`choice-${index}`}
-              onClick={() => handleAnswerQuestion(choice)}
+              onClick={() => handleSelectAnswer(choice)}
             >
               <p
                 id={`choice-${index}`}
@@ -81,7 +82,7 @@ export default function QuizQuestion({
           </li>
         ))}
       </ul>
-      <p className={`${isAnswered ? "hidden" : "block"} text-sm font-bold underline`}>{formatTime(remainingTime)}</p>
+      <p aria-live="polite" className={`${isAnswered ? "hidden" : "block"} text-sm font-bold underline`}>{formatTime(remainingTime)}</p>
       <button
         className={`${isAnswered ? "block" : "hidden"} py-4 px-8 border border-solid border-cs-black rounded-2xl hover:shadow-[0_4px_0_var(--primary)] hover:-translate-y-1 transition-all duration-300`}
         role="button"
